31a5cda2bf432c585c32c23d67b20b85efe30bef
Eliot Howes <ejhowes@gmail.com>
2019-06-30 14:50:25 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,23 +27,50 @@ end
 
 def list(array)
 
+  list_with_numbers = {}
+
   array.each_with_index do |item, index|
+    list_with_numbers["#{index + 1}"] = item
     puts "#{index + 1}. #{item}"
   end
-
+list_with_numbers
 end
 
-binding.pry
 
-def play(array)
+def play(songs)
+  songs_list = list(songs)
 
-  puts "Please enter a song name or number:"
+    puts "Please enter a song name or number:"
+    user_input = gets.chomp
+
+      songs_list.each do |key, value|
+        if user_input == key || value
+          puts "Playing #{value}"
+        end
+      end
+    puts "Invalid input, please try again"
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+
   user_input = gets.chomp
 
-  if user_input > 0 && user_input <= array.length || array.include?(user_input)
-    puts "Playing #{user_input}"
-  else
-    puts "Invalid input, please try again"
+  while user_input != exit do
+    if user_input == "help"
+       help
+    elsif user_input == "list"
+       list(songs)
+    elsif user_input == "play"
+       play(songs)
+    end
+  user_input = gets.chomp
   end
 
 end
